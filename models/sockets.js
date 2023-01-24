b3ccbe142af8312e69c09f0536a8e3c63d3b2564
Ginner Zapata <gzapata@vizzn.ca>
2023-01-24 02:37:16 -0300

    sockets

--- models/sockets.js
+++ models/sockets.js
@@ -1,31 +1,21 @@
-
+const BandList = require("./band-list");
 
 class Sockets {
+  constructor(io) {
+    this.io = io;
+    this.bandList = new BandList();
 
-    constructor( io ) {
+    this.socketEvents();
+  }
 
-        this.io = io;
-
-        this.socketEvents();
-    }
-
-    socketEvents() {
-        // On connection
-        this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
-        
-        });
-    }
-
-
+  socketEvents() {
+    // On connection
+    this.io.on("connection", (socket) => {
+      console.log("Cliente Conectado");
+      //Emitir al cliente conectado, todas las bandas actuales
+      socket.emit("current-bands", this.bandList.getBands());
+    });
+  }
 }
-
 
 module.exports = Sockets;
