a30060b13872ef2281d2e839c7e7c9d5eddab1ab
Ginner Zapata <gzapata@vizzn.ca>
2023-01-24 09:18:52 -0300

    CRUD

--- models/sockets.js
+++ models/sockets.js
@@ -14,6 +14,30 @@ class Sockets {
       console.log("Cliente Conectado");
       //Emitir al cliente conectado, todas las bandas actuales
       socket.emit("current-bands", this.bandList.getBands());
+
+      //votar banda
+      socket.on("vote-band", (id) => {
+        this.bandList.increaseVotes(id);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      //borrar banda
+      socket.on("delete-band", (id) => {
+        this.bandList.removeBand(id);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      //actualizar nombre
+      socket.on("update-name", ({ id, name }) => {
+        this.bandList.changeBandName(id, name);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      //crear banda
+      socket.on("create-band", ({ name }) => {
+        this.bandList.addBand(name);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
     });
   }
 }
